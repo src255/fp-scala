3e7b95dafe2eb33b3e6df3c987d1ecfcd0ec6ab4
Christian Salvati <81280761+src255@users.noreply.github.com>
2022-07-26 19:49:08 -0400

    Remove now redundant imports
    Imports scala.List and scala.Nil are now redundant as the local
    implementation of List has been moved to package hide.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,9 @@
-import scala.List
-import scala.Nil
-
 object Main {
   def main(args: Array[String]): Unit = {
     println(0x2500.toChar.toString * 45)
 
-    val x = List(Some(1), Some(2), None, Some(3), Some(1))
-    println(Option.sequence(x))
+    val x = List(Right(1), Right(2), Right(3), Left("Error"))
+    println(Either.sequence(x))
 
     println(0x2500.toChar.toString * 45)
   }
